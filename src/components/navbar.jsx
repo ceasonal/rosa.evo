2240767dca3b0eec6cd45bc3d58a9113ec8f5c49
ceabenshawn <ceasonalgamer@gmail.com>
2023-12-07 22:59:02 +0530

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,141 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import Badge from "@mui/material/Badge";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import ShoppingCart from "@mui/icons-material/ShoppingCart";
+
+const pages = ["Products", "Customization", "Brochure", "AboutUs", "ContactUs"];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static" style={{ backgroundColor: "inherit", color: "#4D1F08", boxShadow: "none", }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            rosa.evo
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                 <Typography textAlign="center" style={{ color: "#4D1F08", fontFamily: "monospace" }}>{page}</Typography>
+                </MenuItem>
+              ))}
+              <MenuItem onClick={handleCloseNavMenu}>
+              <Typography textAlign="center" style={{ color: "#4D1F08", fontFamily: "monospace" }}>Account</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            rosa.evo
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "#4D1F08", display: "block", textTransform: "none", fontSize: "14px" }}
+              > 
+                {page}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+          <IconButton
+            size="large"
+            aria-label="show no. of cart items"
+            color="inherit"
+          >
+            <Badge badgeContent={17} color="error">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default ResponsiveAppBar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from '../components/navbar'
+const Home = () => {
+  return (
+    <>
+    <Nav/>
+        <h1>Home</h1>
+    </>
+  )
+}
+
+export default Home
